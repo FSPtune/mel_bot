bcc754ae78f40130ce144c50442869a86c9bf566
Sergey Melnikov <fsptune@gmail.com>
2022-11-28 13:15:20 +0300

    proxi del

--- mel_bot.py
+++ mel_bot.py
@@ -5,20 +5,20 @@ import settings
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 
-PROXY = {'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
+#PROXY = {'proxy_url': settings.PROXY_URL,
+#    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}} 
 
 def greet_user(update, context):
     print("Вызван /start")
-    update.message.reply_text("Здравствуй, пользователь!")
+    update.message.reply_text("Здрuавствуй, пользователь!")
 
 def talk_to_me(update, context):
     text = update.message.text
-    print(text)
+    print(text, update)
     update.message.reply_text(text)
 
 def main():
-    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
